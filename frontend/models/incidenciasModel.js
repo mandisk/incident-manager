4dff76f5121cc3c6dd282853f04c2928a8d3f879
Manuel Valls Vorndran <mandisk@Mac-mini-de-Manuel.local>
2016-01-11 01:09:58 +0100

    Añadido formulario de pruebas

--- frontend/models/incidenciasModel.js
+++ frontend/models/incidenciasModel.js
@@ -1,4 +1,11 @@
-//modelo notasModel
+//modelo 
 //un servicio viene a ser un modelo
 Incidence.service("incidencesModel", function(){
+incidence = {
+    name: "",
+    description: "",
+    author: "",
+    priority: "",
+    state: ""
+  };
 });
